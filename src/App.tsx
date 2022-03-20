a58d9233d66cd7cc068c573656eed06304b4786f
Michal Krokosz <michal.krokosz@wallee.com>
2022-03-20 22:52:43 +0100

    initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Text, View, TextInput} from 'react-native';
+import { EmvDecoder } from './EmvDecoder';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const valueFromUrl = new URLSearchParams(window.location.search)?.get("value");
+    const initialRsult = (valueFromUrl)? EmvDecoder.processValue(valueFromUrl) : "";
+    const [result, setResult] = useState(EmvDecoder.processValue(initialRsult));
+
+    return (
+        <View >
+            <View>
+                <br></br>
+                <TextInput
+                    defaultValue={valueFromUrl || undefined}
+                    placeholder="Type/paste a hex or base64 value"
+                    onChangeText={inputValue => { setResult(EmvDecoder.processValue(inputValue)) } }
+                />
+
+                <Text>
+                    <br></br> {result}
+                </Text>
+            </View>
+        </View>
+    );
+};
 
 export default App;
---
+++ src/EmvDecoder.ts
@@ -0,0 +1,106 @@
+import { Buffer } from "buffer";
+import tags from './emvtags.json';
+
+
+interface EmvTag {
+    name: string;
+    tag?: string;
+    length: number;
+    bits: string[];
+}
+
+export class EmvDecoder {
+
+    private static validateTagsArray(tagsArray: EmvTag[]) {
+        for(const tag of tagsArray) {
+            if (tag.bits.length / 8 != tag.length) {
+                return tag.name || "Unknown tag";
+            }
+        }
+        return undefined;
+    }
+    private static decodeTag(buf: Buffer, emvTag: EmvTag): string {
+        let res = `${emvTag.name}:\n`;
+        if (emvTag.tag) {
+            res += `TAG: ${emvTag.tag}`;
+        }
+        for (let i = 0; i < emvTag.bits.length; i++) {
+            const byte = Math.floor(i / 8) + 1;
+            const bit = i % 8 + 1;
+            if (buf[byte - 1] & (1 << (bit - 1))) {
+                res += `Byte ${byte} bit ${bit}: ${emvTag.bits[i]}\n`;
+            }
+        }
+        res += "\n\n";
+        return res;
+    }
+    private static decodeTags(buf: Buffer): string {
+        let res = "";
+        for(const tag of tags.emvTags) {
+            if (tag.length == buf.length) {
+                res += this.decodeTag(buf, tag);
+            }
+        }
+        return res;
+    }
+
+    static processValue(value: string): string {
+        if (!value) {
+            return "";
+        }
+        const base64Regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+        const hexRegex = /^[0-9a-fA-F]+$/;
+
+        let base64Value: string | undefined;
+        let hexValue: string | undefined;
+
+        if (base64Regex.test(value)) {
+            base64Value = value;
+        }
+
+        if (hexRegex.test(value) && value.length % 2 == 0) {
+            hexValue = value;
+        }
+
+        if (!hexValue && !base64Value) {
+            return `Can't recognize encoding for value: ${value}`;
+        }
+
+        const recognizedEncoding = (hexValue)? "HEX" : "BASE64";
+
+        let result = "\n\n";
+
+        if (hexValue && base64Value) {
+            result += "WARNING: THE ENTERED VALUE COULD EITHER BE HEX, or BASE64\n";
+        }
+
+
+        result += `Assumed source encoding: ${recognizedEncoding}\n`;
+
+
+
+        let buf: Buffer | undefined;
+
+        if (hexValue && hexValue.length % 2 == 0) {
+            buf = Buffer.from(hexValue, "hex");
+            base64Value = buf.toString("base64");
+            result += `B64 value: ${base64Value}\n`;
+        } else {
+            buf = Buffer.from(value, "base64");
+            hexValue = buf.toString("hex");
+            result += `HEX value: ${base64Value}\n`;
+        }
+
+        result += "\n\n";
+
+        const invalidTag = this.validateTagsArray(tags.emvTags);
+
+        if (invalidTag) {
+            result += `ERROR: Invalid definition of tag ${invalidTag}`;
+        } else {
+            result += this.decodeTags(buf);
+        }
+
+        return result;
+    }
+}
